19e3ce86a37d62a8c796fe4ea6afbb1c3ed2a228
Guilherme <walrusbane524@gmail.com>
2023-12-10 13:43:38 -0300

    object picking

--- headers/object.h
+++ headers/object.h
@@ -4,7 +4,12 @@
 #include "color.h"
 #include "lit_point.h"
 #include "material.h"
-#include "matrix.h"
+#include "rotation_matrix_u_axis.h"
+#include "rotation_matrix_x_axis.h"
+#include "rotation_matrix_y_axis.h"
+#include "rotation_matrix_z_axis.h"
+#include "translation_matrix.h"
+#include "scale_matrix.h"
 #include <optional>
 #include <string>
 #include <iostream>
@@ -19,4 +24,7 @@ class Object{
         virtual optional<LitPoint> colide(Ray ray) const = 0;
         
         virtual void info() = 0;
+        virtual void translate() = 0;
+        virtual void rotate() = 0;
+        virtual void scale() = 0;
 };
